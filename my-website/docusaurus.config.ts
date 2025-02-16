01e55494b017f069d637910239d5c11428fd91a0
avivv3 <aviv@tres.finance>
2025-02-17 00:31:56 +0200

    fix: update copyright information in docusaurus configuration

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -126,7 +126,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Tres Finance, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
