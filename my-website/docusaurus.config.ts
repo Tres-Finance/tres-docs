fcdf542bcd4857b7ee1d1527dc43b54c4cf97823
avivv3 <aviv@tres.finance>
2025-02-17 00:09:14 +0200

    fix: update production URL and baseUrl in docusaurus configuration

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/Triangle.png',
 
   // Set the production url of your site here
-  url: 'https://tres-finance.github.io',
+  url: ' https://tres-finance.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tres-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
