fe6cc70da67a5d1c9479ffddf2f3b3c5b05d377a
avivv3 <aviv@tres.finance>
2025-02-16 19:31:19 +0200

    Update link text and destination in homepage

--- my-website/src/pages/index.tsx
+++ my-website/src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/overview">
+            Explore our API 🔎
           </Link>
         </div>
       </div>
